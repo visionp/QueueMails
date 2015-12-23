345089a2a9e5266d2798ca7431ed66de8fb92ee4
visionp <Opetrenk0>
2015-12-23 11:09:33 +0200

    Initial commit

---
+++ MyMessages.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VisioN
+ * Date: 21.12.2015
+ * Time: 17:26
+ */
+
+namespace vision\queue_mails;
+
+use Yii;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+
+
+class MyMessages extends Component {
+
+}
